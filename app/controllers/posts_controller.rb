3812a1e742dd144c7d0d0efaac60f67c1b1c4403
bebofofum <bri.boehm@gmail.com>
2020-10-06 20:24:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+
   def show
     @post = Post.find(params[:id])
   end
@@ -28,10 +30,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = set_post!
+    @post.update(post_params)    
 
     if @post.valid? 
-       @post.update(post_params)    
        redirect_to post_path(@post)
     else
       render :edit
